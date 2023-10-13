c707e980a6bcb64ca6f2f19538304ed38262d535
21SINGH <s.far.focus@gmail.com>
2023-10-13 19:35:50 +0530

    new

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,12 @@
 "use server"
 
 import { revalidatePath } from "next/cache";
-import Product from "../models /product.model";
+import Product from "../models/product.model";
 import { connectToDB } from "../scraper/mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../scraper/utils";
 import { User } from "@/types";
-import { generateEmailBody, sendEmail } from "../nodemailer ";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if(!productUrl) return;
