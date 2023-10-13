0d15739dc8433012314418b73a1b46b43547a727
21SINGH <s.far.focus@gmail.com>
2023-10-13 19:45:17 +0530

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/scraper/utils";
 import { connectToDB } from "@/lib/scraper/mongoose";
-import Product from "@/lib/models /product.model";
+import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
-import { generateEmailBody, sendEmail } from "@/lib/nodemailer ";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
